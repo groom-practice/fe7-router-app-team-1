926786c90698f31a5a49b12019ec556638f9a053
uzzin <01uzzin@gmail.com>
2025-12-12 11:27:10 +0900

    ✨feat: #2 무한스크롤

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import '../styles/PostList.css';
 import getAllPosts from '../apis/getAllPosts';
 import deletePost from '../apis/deletePost';
@@ -6,27 +6,74 @@ import { createPortal } from 'react-dom';
 import { Link } from 'react-router-dom';
 
 export default function PostList() {
-  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]); // 전체 데이터
+  const [posts, setPosts] = useState([]); // 화면에 보여지는 데이터
+  const [page, setPage] = useState(1); // 현재 페이지
+  const postsPerPage = 10; // 한 페이지에 보여줄 데이터 수
+
+  const loaderRef = useRef(null); // 무한스크롤 감지용 DOM 요소
+  const listRef = useRef(null);
+
   const [openModal, setOpenModal] = useState(null);
 
   useEffect(() => {
     getAllPosts().then((res) => {
-      setPosts(res);
+      setAllPosts(res);
+      setPosts(allPosts.slice(0, postsPerPage)); // 처음 렌더링 10개 데이터만
     });
   }, []);
+
+  useEffect(() => {
+    if (!listRef.current) return;
+
+    const items = listRef.current.querySelectorAll('li');
+    items.forEach((item) => {
+      setTimeout(() => {
+        item.classList.add('fade-in');
+      }, 500);
+    });
+  }, [posts]);
+
+  // IntersectionObserver 설정
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          const nextPage = page + 1;
+          const nextPosts = allPosts.slice(0, nextPage * postsPerPage);
+
+          if (nextPosts.length !== posts.length) {
+            setPosts(nextPosts);
+            setPage(nextPage);
+          }
+        }
+      },
+      { root: null, rootMargin: '0px', threshold: 1.0 }
+    );
+
+    // loaderRef div 관찰
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    // 컴포넌트 언마운트 시 옵저버 해제
+    return () => {
+      if (loaderRef.current) observer.unobserve(loaderRef.current);
+    };
+  }, [page, allPosts]);
 
   const handleDelete = async () => {
     if (openModal === null) return;
 
     await deletePost(openModal);
     setOpenModal(null);
-    setPosts((prev) => prev.filter((p) => p.id !== openModal));
+    setAllPosts((prev) => prev.filter((p) => p.id !== openModal));
   };
 
   return (
     <div className="postListSection">
       <h3>Posts Lists</h3>
-      <ul>
+      <ul ref={listRef}>
         {posts.map((post) => (
           <li key={post.id}>
             <Link to={`/posts/${post.id}`}>
@@ -36,6 +83,8 @@ export default function PostList() {
           </li>
         ))}
       </ul>
+
+      <div ref={loaderRef}></div>
 
       {openModal &&
         createPortal(
