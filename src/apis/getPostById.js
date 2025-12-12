6eb16e551eda725f85e16e8f0a92c79147e62683
Leemainsw <yebom6476@gmail.com>
2025-12-12 10:42:40 +0900

    ✨feat: #3 post 상세 조회 API 추가

---
+++ src/apis/getPostById.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+export async function getPostById(postId) {
+  const response = await fetch(`${API_URL}/${postId}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch post');
+  }
+
+  const post = await response.json();
+  return post;
+}
